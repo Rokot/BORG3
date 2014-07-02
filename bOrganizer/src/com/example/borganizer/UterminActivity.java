21b181d893423e6f78798ffdcb43c26eecc01992
R-R <R-R@R-R-Acer>
2014-07-02 16:34:05 +0200

    Version5 Ivan

--- bOrganizer/src/com/example/borganizer/UterminActivity.java
+++ bOrganizer/src/com/example/borganizer/UterminActivity.java
@@ -32,7 +32,7 @@ import android.view.View;
 import android.widget.AdapterView.AdapterContextMenuInfo;
 import android.widget.ListView;
 
-public class UterminActivity extends FragmentActivity implements LoaderCallbacks<Cursor>{
+public class UterminActivity extends Activity {
 	
 	private static final int CM_DELETE_ID = 1;
 	  ListView lvData;
@@ -63,24 +63,7 @@ public class UterminActivity extends FragmentActivity implements LoaderCallbacks
 			dlgAlert.setIcon(R.drawable.ic_launcher);
 			dlgAlert.show();
 ////////////////////////////////////////////////////////////			
-			db = new DB(this);
-		    db.open();
-		    
-	
-		    String[] from = new String[] { DB.COLUMN_IMG, DB.COLUMN_TXT };
-		    int[] to = new int[] { R.id.ivImg, R.id.tvText };
-
-		  
-		    scAdapter = new SimpleCursorAdapter(this, R.layout.fragment_utermin, null, from, to, 0);
-		    lvData = (ListView) findViewById(R.id.lvData);
-		    lvData.setAdapter(scAdapter);
-
-		 
-		    registerForContextMenu(lvData);
-		    
-		  
-		    getSupportLoaderManager().initLoader(0, null, this);
-			
+		
 		}
 	}
 
@@ -151,74 +134,6 @@ public class UterminActivity extends FragmentActivity implements LoaderCallbacks
 	}
 //////////////////////////////////////////////////////
 	
-	 
-	  public void onButtonClick(View view) {
-	
-	    db.addRec("sometext " + (scAdapter.getCount() + 1), R.drawable.ic_launcher);
-	    
-	    getSupportLoaderManager().getLoader(0).forceLoad();
-	  }
-	  
-	  public void onCreateContextMenu(ContextMenu menu, View v,
-	      ContextMenuInfo menuInfo) {
-	    super.onCreateContextMenu(menu, v, menuInfo);
-	    menu.add(0, CM_DELETE_ID, 0, R.string.delete_record);
-	  }
-
-	  public boolean onContextItemSelected(MenuItem item) {
-	    if (item.getItemId() == CM_DELETE_ID) {
-	
-	      AdapterContextMenuInfo acmi = (AdapterContextMenuInfo) item
-	          .getMenuInfo();
-	
-	      db.delRec(acmi.id);
-	 
-	      getSupportLoaderManager().getLoader(0).forceLoad();
-	      return true;
-	    }
-	    return super.onContextItemSelected(item);
-	  }
-
-	  protected void onDestroy() {
-	    super.onDestroy();
-	   
-	    db.close();
-	  }
-
-	  @Override
-	  public Loader<Cursor> onCreateLoader(int id, Bundle bndl) {
-	    return new MyCursorLoader(this, db);
-	  }
-
-	  @Override
-	  public void onLoadFinished(Loader<Cursor> loader, Cursor cursor) {
-	    scAdapter.swapCursor(cursor);
-	  }
-
-	  @Override
-	  public void onLoaderReset(Loader<Cursor> loader) {
-	  }
-	  
-	  static class MyCursorLoader extends CursorLoader {
-
-	    DB db;
-	    
-	    public MyCursorLoader(Context context, DB db) {
-	      super(context);
-	      this.db = db;
-	    }
-	    
-	    @Override
-	    public Cursor loadInBackground() {
-	      Cursor cursor = db.getAllData();
-	      try {
-	        TimeUnit.SECONDS.sleep(3);
-	      } catch (InterruptedException e) {
-	        e.printStackTrace();
-	      }
-	      return cursor;
-	    }
-	  }
 	    ///////////////////////////////////////////////////////////////////////////
 	/**
 	 * A placeholder fragment containing a simple view.
